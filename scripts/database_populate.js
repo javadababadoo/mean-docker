74fc1d25eefaba87b0da23e7f350725b956a6897
Felipe Santa <juan.santa@nebulae.com.co>
2018-07-11 13:45:09 -0500

    testing fork

--- scripts/database_populate.js
+++ scripts/database_populate.js
@@ -1,10 +1,13 @@
+function createRandomDevices() {
+  db = db.getSiblingDB("DeviceDB");
 
-function createRandomDevices(){
-    db = db.getSiblingDB("DeviceDB");
+  for (i = 0; i < 100; i++) {
+    var res = db.devices.insertOne({
+      name: "Dispositivo " + i,
+      type: "T",
+      desc: "Dispositivo de prueba " + i
+    });
+  }
 
-for (i = 0; i < 100; i++) { 
-    var res = db.devices.insertOne({name: "Dispositivo "+i, type: "T", desc: "Dispositivo de prueba "+i});
-}
-
-  console.log('Tarea terminada');
+  console.log("Tarea terminada");
 }
